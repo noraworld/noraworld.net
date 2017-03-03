3d7f497339270888a1745c9fccf701a0921e7b52
noraworld <noraworld@mail.com>
2017-03-03 17:29:31 +0900

    :recycle: Fix indents

--- app/js/webrtc.js
+++ app/js/webrtc.js
@@ -1,10 +1,10 @@
+// referred to https://github.com/diafygi/webrtc-ips. thanks!
 function getIPs(callback) {
   var ip_dups = {};
-
   var RTCPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
   var useWebKit = !!window.webkitRTCPeerConnection;
 
-  if(!RTCPeerConnection) {
+  if (!RTCPeerConnection) {
     var win = iframe.contentWindow;
     RTCPeerConnection = win.RTCPeerConnection || win.mozRTCPeerConnection || win.webkitRTCPeerConnection;
     useWebKit = !!win.webkitRTCPeerConnection;
@@ -14,39 +14,39 @@ function getIPs(callback) {
     optional: [{RtpDataChannels: true}]
   };
   var servers = {iceServers: [{urls: "stun:stun.services.mozilla.com"}]};
-
   var pc = new RTCPeerConnection(servers, mediaConstraints);
+
   function handleCandidate(candidate) {
     var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
     var ip_addr = ip_regex.exec(candidate)[1];
 
-    if (ip_dups[ip_addr] === undefined)
+    if (ip_dups[ip_addr] === undefined) {
       callback(ip_addr);
-      ip_dups[ip_addr] = true;
     }
+    ip_dups[ip_addr] = true;
+  }
 
-    pc.onicecandidate = function(ice) {
-      if (ice.candidate)
-        handleCandidate(ice.candidate.candidate);
-    };
-
-    pc.createDataChannel("");
-
-    pc.createOffer(function(result){
-
+  pc.onicecandidate = function(ice) {
+    if (ice.candidate) {
+      handleCandidate(ice.candidate.candidate);
+    }
+  };
+  pc.createDataChannel("");
+  pc.createOffer(function(result) {
     pc.setLocalDescription(result, function(){}, function(){});
   }, function(){});
 
   setTimeout(function() {
     var lines = pc.localDescription.sdp.split('\n');
-    lines.forEach(function(line){
-      if(line.indexOf('a=candidate:') === 0)
+    lines.forEach(function(line) {
+      if (line.indexOf('a=candidate:') === 0) {
         handleCandidate(line);
+      }
     });
   }, 1000);
 }
 
-getIPs(function(ip){
+getIPs(function(ip) {
   var localIP = document.getElementById('local-ip-val');
 
   if (ip.match(/^(192\.168\.|169\.254\.|10\.|172\.(1[6-9]|2\d|3[01]))/)) {
