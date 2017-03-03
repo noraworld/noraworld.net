f3970b8ffc663fceaaf23c038e17e962ae426119
noraworld <noraworld@mail.com>
2017-03-03 17:14:53 +0900

    :sparkles: Deal with client real values

--- app/index.php
+++ app/index.php
@@ -1,4 +1,5 @@
 <?php
+  // load from browser detector
   require '../vendor/autoload.php';
   use Sinergi\BrowserDetector\Browser;
   use Sinergi\BrowserDetector\Os;
@@ -8,6 +9,43 @@
   $os       = new Os();
   $device   = new Device();
   $language = new Language();
+
+  // os information
+  if ($os->getName() !== Os::UNKNOWN && $os->getVersion() !== Os::UNKNOWN) {
+    $os_result = $os->getName() . ' ' . $os->getVersion();
+  }
+  elseif ($os->getName() !== Os::UNKNOWN && $os->getVersion() === Os::UNKNOWN) {
+    $os_result = $os->getName();
+  }
+  else {
+    $os_result = "Unknown";
+  }
+
+  // browser information
+  if ($browser->getName() !== Browser::UNKNOWN && $browser->getVersion() !== Browser::UNKNOWN) {
+    $browser_result = $browser->getName() . ' ' . $browser->getVersion();
+  }
+  elseif ($browser->getName() !== Browser::UNKNOWN && $browser->getVersion() === Browser::UNKNOWN) {
+    $browser_result = $browser->getName();
+  }
+  else {
+    $browser_result = "Unknown";
+  }
+
+  // device information
+  if ($device->getName() !== Device::UNKNOWN) {
+    $device_result = $device->getName();
+  }
+  else {
+    $device_result = "Unknown";
+  }
+
+  if ($language->getLanguage() !== "") {
+    $language_result = $language->getLanguage();
+  }
+  else {
+    $language_result = "None";
+  }
 ?>
 <?php include './template/header.php'; ?>
   <div class="contents clearfix">
@@ -16,36 +54,64 @@
       <p class="data"><?= $_SERVER["REMOTE_ADDR"]; ?></p>
     </div>
     <div class="content col-2">
-      <p class="title">Local IP address</p>
-      <p class="data"><?= "192.168.33.10" ?></p>
+      <p id="local-ip" class="title">Local IP address</p>
+      <p id="local-ip-val" class="data"><noscript>JavaScript is disabled</noscript></p>
     </div>
     <div class="content col-1">
       <p class="title">Remote host</p>
-      <p class="data"><?= "remotehost.net"; ?></p>
+      <?php if ($_SERVER["REMOTE_HOST"] !== null): ?>
+        <p class="data"><?= $_SERVER["REMOTE_HOST"]; ?></p>
+      <?php else: ?>
+        <p class="data grey">None</p>
+      <?php endif ?>
     </div>
     <div class="content col-2">
       <p class="title">OS</p>
-      <p class="data"><?= $os->getName(); ?> <?= $os->getVersion(); ?></p>
+      <?php if ($os_result !== "Unknown"): ?>
+        <p class="data"><?= $os_result; ?></p>
+      <?php else: ?>
+        <p class="data grey"><?= $os_result; ?></p>
+      <?php endif ?>
     </div>
     <div class="content col-2">
       <p class="title">Browser</p>
-      <p class="data"><?= $browser->getName(); ?> <?= $browser->getVersion(); ?></p>
+      <?php if ($browser_result !== "Unknown"): ?>
+        <p class="data"><?= $browser_result; ?></p>
+      <?php else: ?>
+        <p class="data grey"><?= $browser_result; ?></p>
+      <?php endif ?>
     </div>
     <div class="content col-2">
       <p class="title">Device</p>
-      <p class="data"><?= $device->getName(); ?></p>
+      <?php if ($device_result !== "Unknown"): ?>
+        <p class="data"><?= $device_result; ?></p>
+      <?php else: ?>
+        <p class="data grey"><?= $device_result; ?></p>
+      <?php endif ?>
     </div>
     <div class="content col-2">
       <p class="title">Language</p>
-      <p class="data"><?= $language->getLanguage(); ?></p>
+      <?php if ($language_result !== "None"): ?>
+        <p class="data"><?= $language_result; ?></p>
+      <?php else: ?>
+        <p class="data grey"><?= $language_result; ?></p>
+      <?php endif ?>
     </div>
     <div class="content col-1">
       <p class="title">Referrer</p>
-      <p class="data"><?= $_SERVER["HTTP_REFERER"]; ?></p>
+      <?php if ($_SERVER["HTTP_REFERER"] !== null): ?>
+        <p class="data"><?= $_SERVER["HTTP_REFERER"] ?></p>
+      <?php else: ?>
+        <p class="data grey">None or disabled</p>
+      <?php endif ?>
     </div>
     <div class="content col-1">
       <p class="title">User Agent</p>
-      <p class="data"><?= $_SERVER["HTTP_USER_AGENT"]; ?></p>
+      <?php if ($_SERVER["HTTP_USER_AGENT"] !== null): ?>
+        <p class="data"><?= $_SERVER["HTTP_USER_AGENT"] ?></p>
+      <?php else: ?>
+        <p class="data grey">None or disabled</p>
+      <?php endif ?>
     </div>
   </div>
 <?php include './template/footer.php'; ?>
--- app/template/footer.php
+++ app/template/footer.php
@@ -1,5 +1,6 @@
   <footer class="clearfix">
     <section class="copyright">Copyright &copy; 2017 <a href="/">noraworld.net</a>. All rights reserved.</section>
   </footer>
+  <script type="text/javascript" src="/js/webrtc.js"></script>
 </body>
 </html>
