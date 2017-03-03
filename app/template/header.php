aa0f6f80232231e3fb93a7c805b8583b71c63bf9
noraworld <noraworld@mail.com>
2017-03-03 19:23:41 +0900

    :cherry_blossom: Import font files and apply these

--- app/template/header.php
+++ app/template/header.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>noraworld.net</title>
   <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/css/font.css">
 </head>
 <body>
   <header>
